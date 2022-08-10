f5d7e8971cf400be332d4b80c3c1a6a67902ab26
Arthur <110948046+onybrxc@users.noreply.github.com>
2022-08-10 08:44:14 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "klinegrid.h"
+#include "kvolumegrid.h"
+#include <QSplitter>
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{   
+    ui->setupUi(this);
+
+
+
+
+    pgrid = new kVolumeGrid(this);
+    pgrid->setObjectName(tr("kline"));
+    pgrid->setFocusPolicy(Qt::StrongFocus);
+
+    auto p2 = new KLineGrid(this);
+    p2->setFocusPolicy(Qt::StrongFocus);
+
+   // setCentralWidget(pgrid);
+
+
+
+    QSplitter *splitterMain = new QSplitter(Qt::Vertical, 0); //新建主分割窗口，水平分割
+    QSplitter *splitterLeft = new QSplitter(Qt::Vertical, splitterMain);
+    QSplitter *splitterRight = new QSplitter(Qt::Vertical, splitterMain);
+
+
+    splitterMain->setHandleWidth(1);
+
+    splitterLeft->addWidget(p2);
+    splitterRight->addWidget(pgrid);
+    this->setCentralWidget(splitterMain);
+
+
+    resize(1200,800);
+
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
